7c089cbbc095d7812202077f4c688c31c0fc4627
Cabap8126 <cabap8126@gmail.com>
2025-11-23 13:54:33 +0700

    h1

--- text2.js
+++ text2.js
@@ -1,10 +1,10 @@
 const buttons = document.querySelectorAll(".button-on img");
-console.log(buttons);
 const image = document.getElementsByTagName("img");
-console.log(image);
-const imageClick = document.getElementsByClassName("inner-image1");
 buttons.forEach(img=>{
     img.addEventListener("mouseenter",()=>{
         image[3].src = img.src
+    })
+    img.addEventListener("mouseleave",()=>{
+        image[3].src = "";
     })
 })
